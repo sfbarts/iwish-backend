e3ca00e9e12dec99949e47398e893716d010960b
sfbarts <sfbedu@gmail.com>
2023-09-20 10:32:22 -0400

    Save the total price of the wishlist
    -Current method does a lookup on the items collection and then sums the prices of the items and merges
    them back to the wishlists collection. It does so when adding an item.
    -Future method might involve doing a put request to the specific wishlist and update its total
    when closing the list on the frontend.

--- controllers/items.js
+++ controllers/items.js
@@ -1,5 +1,6 @@
 const itemsRouter = require('express').Router()
 const Item = require('../models/item')
+const Wishlist = require('../models/wishlist')
 
 //Get list of items
 itemsRouter.get('/', async (request, response) => {
@@ -19,9 +20,6 @@ itemsRouter.get('/:wishlistId', async (request, response) => {
 itemsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  //temporary category id
-  // const wishlistID = '650a1a9d561cd48a13a4f05a'
-
   const item = new Item({
     name: body.name,
     url: body.url,
@@ -30,6 +28,29 @@ itemsRouter.post('/', async (request, response) => {
   })
 
   const addItem = await item.save()
+
+  //Provisional way to save the wishlist total
+  const total = await Wishlist.aggregate([
+    {
+      $lookup: {
+        from: 'items',
+        localField: '_id',
+        foreignField: 'wishlist',
+        as: 'items',
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        total: {
+          $sum: '$items.price',
+        },
+      },
+    },
+    {
+      $merge: 'wishlists', // Save the results back to the 'wishlists' collection
+    },
+  ])
 
   response.status(201).json(addItem)
 })
