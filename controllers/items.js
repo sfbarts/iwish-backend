3d1d1796c798d6108b1edc31313d51857efa0223
sfbarts <sfbedu@gmail.com>
2023-09-20 09:50:17 -0400

    Get items based on wishlist id
    -Implement a route to GET /api/items/:wishlistId to retrieve items based on wishlist
    -Modify route POST /api/items to use the wishlist id inside the request body

--- controllers/items.js
+++ controllers/items.js
@@ -7,18 +7,26 @@ itemsRouter.get('/', async (request, response) => {
   response.json(items)
 })
 
+//Get list of items based on the wishlist id
+itemsRouter.get('/:wishlistId', async (request, response) => {
+  const items = await Item.find({
+    wishlist: request.params.wishlistId,
+  }).populate('wishlist', { name: 1 })
+  response.json(items)
+})
+
 //Add new item
 itemsRouter.post('/', async (request, response) => {
   const body = request.body
 
   //temporary category id
-  const wishlistID = '650a1a9d561cd48a13a4f05a'
+  // const wishlistID = '650a1a9d561cd48a13a4f05a'
 
   const item = new Item({
     name: body.name,
     url: body.url,
     price: Number(body.price),
-    wishlist: wishlistID,
+    wishlist: body.wishlist,
   })
 
   const addItem = await item.save()
