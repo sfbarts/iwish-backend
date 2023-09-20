c35e02dbe0e8546d77ca8fb8e74c5f4a89ec7d32
sfbarts <sfbedu@gmail.com>
2023-09-20 09:59:31 -0400

    Get wishlists based on category id
    -Add route to GET api/wishlists/:categoryId to retrieve wishlists by category
    -Modify POST api/wishlists to use categoryId from the request body

--- controllers/wishlists.js
+++ controllers/wishlists.js
@@ -7,16 +7,24 @@ wishlistsRouter.get('/', async (request, response) => {
   response.json(wishlists)
 })
 
+//Get list of wishlists based on category id
+wishlistsRouter.get('/:categoryId', async (request, response) => {
+  const wishlists = await Wishlist.find({
+    category: request.params.categoryId,
+  }).populate('category', { name: 1 })
+  response.json(wishlists)
+})
+
 //Add new wishlist
 wishlistsRouter.post('/', async (request, response) => {
   const body = request.body
 
   //temporary category id
-  const categoryID = '650a18246081432e28194a05'
+  // const categoryID = '650a18246081432e28194a05'
 
   const wishlist = new Wishlist({
     name: body.name,
-    category: categoryID,
+    category: body.category,
   })
 
   const addWishlist = await wishlist.save()
