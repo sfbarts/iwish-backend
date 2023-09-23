647b41b8bc5ad0726531712bcc9b85917ef11482
sfbarts <sfbedu@gmail.com>
2023-09-23 07:49:02 -0400

    Update Categories Controller -Add route to get categories by user id. GET at api/categories/:userId. Temporarily using userId from route params. -Add user from body request to post request -Add update route PUT at api/categories/:categoryId to update names. -Add delete route DELETE at api/categories/:categoryId to delete categories.

--- controllers/categories.js
+++ controllers/categories.js
@@ -7,12 +7,21 @@ categoriesRouter.get('/', async (request, response) => {
   response.json(categories)
 })
 
+//Get list of categories by userId
+categoriesRouter.get('/:userId', async (request, response) => {
+  //temporarily return the userid
+  const categories = await Category.find({
+    user: request.params.userId,
+  }).populate('user', { name: 1, id: 1 })
+  response.json(categories)
+})
+
 //Add new category
 categoriesRouter.post('/', async (request, response) => {
   const body = request.body
 
   //temporary user id
-  let userID = '650a14c573d510a95e0fb3bf'
+  let userID = body.user
 
   const category = new Category({
     name: body.name,
@@ -22,6 +31,29 @@ categoriesRouter.post('/', async (request, response) => {
   const addCategory = await category.save()
 
   response.status(201).json(addCategory)
+})
+
+categoriesRouter.put('/:categoryId', async (request, response) => {
+  const body = request.body
+
+  const category = {
+    name: body.name,
+  }
+
+  const updatedCategory = await Category.findByIdAndUpdate(
+    request.params.categoryId,
+    category,
+    {
+      new: true,
+      context: 'query',
+    }
+  )
+  response.json(updatedCategory)
+})
+
+categoriesRouter.delete('/:categoryId', async (request, response) => {
+  await Category.findByIdAndDelete(request.params.categoryId)
+  return response.status(204).end()
 })
 
 module.exports = categoriesRouter
